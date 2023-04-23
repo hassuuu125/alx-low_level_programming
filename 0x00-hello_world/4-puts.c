f08780a1600a608ffebe6b652772011f4b02534d
hassuuu125 <ermiashasset123@gmail.com>
2023-04-23 03:32:00 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #!/bin/bash
-#include<stido.h>
+#include <stido.h>
 /**
 * main - A program that prints a line with puts function
 * Return: 0 (success)
 */
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle")
-   return(0);
+ puts("\"Programming is like building a multilingual puzzle")
+return (0);	 
 }   
