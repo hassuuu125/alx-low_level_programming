e933ec2315fa6d7a7ea4862c84b67254c51057ff
hassuuu125 <ermiashasset123@gmail.com>
2023-04-23 02:48:20 -0700

    updated

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stido.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+   puts("\"Programming is like building a multilingual puzzle")
+   return(0);
+}   
