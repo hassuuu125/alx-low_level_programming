da106fb1b32b77663a7b9e67630bc9afb058cd05
hassuuu125 <ermiashasset123@gmail.com>
2023-04-23 13:21:45 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * description:main - A program that prints a line with puts function
-* Return: 0 if success
+* Return: 0 (success)
 */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle,\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
