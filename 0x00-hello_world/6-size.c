b0f3dea56fb46cea59b93af5a9eb7dcd381d5948
hassuuu125 <ermiashasset123@gmail.com>
2023-04-23 05:38:28 -0700

    updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,19 @@
-#!/bin/bash
-#includ<stido.h>
+#includ < stido.h >
 /**
-*main - Entry point
-*
-* Return:Always 0 (success)
-*/
+ *main - A program that prints the size of various computer types
+ *Return: 0 (success)
+ */
 int main(void)
 {
-printf("size of a char: %d byte(s)\n",sizeof(char));
-printf("size of an int: %d byte(s)\n",sizeof(int));
-printf("size of a long int: %d byte(s)\n",sizeof(long int));
-printf("size of long long int: %d byte(s)\n",sizeof(long long int));
-printf("size of float: %d byte(s)\n",sizeof(float));
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu  byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu  byte(s)\n", (unsigned long)sizeof(e));
 return (0);
-	
+}
