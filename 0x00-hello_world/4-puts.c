71a94816341bfa2960f8c0d78a88144638a2816b
hassuuu125 <ermiashasset123@gmail.com>
2023-04-23 03:26:24 -0700

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #!/bin/bash
-#include<stido.h>
+#include <stido.h>
 /**
-*main - Entry point
-*
-* Return: Always 0 (success)
+* main - A program that prints a line with puts function
+* Return: 0 (success)
 */
 int main(void)
 {
