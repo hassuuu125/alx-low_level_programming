5903623b9b6cffa6a2755fd28fcf90bcb933b684
hassuuu125 <ermiashasset123@gmail.com>
2023-04-23 05:58:41 -0700

    updated

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,11 @@
-#!/bin/bash
-#include<stdio.h>
-#include<unisted.h>
-
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Entry point
- *
+ * main - A c program that prints a line to the standard error
  * Return: Always 1 (success)
  */
 int main(void)
 {
-     write(2,
-	     "and that piece of art is useful" - Dora Korpar, 2015-10-19\n" ,59);
-      return(1);
-}     
+ fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return (1);
+}
