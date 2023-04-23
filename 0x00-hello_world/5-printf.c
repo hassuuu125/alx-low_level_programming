e001077880701efd7b17871432e0f98a2e8c4c70
hassuuu125 <ermiashasset123@gmail.com>
2023-04-23 05:03:02 -0700

    updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
-#!/bin/bash
-#include<stido.h>
+#include <stido.h>
 /**
-*main - Entry point
-*
-* Return: Always 0 (success)
-*/
+ * main - A program that print a line using a printf function
+ * Return: 0 (success)
+ */
+
 int main(void);
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return(0);
-}    
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
